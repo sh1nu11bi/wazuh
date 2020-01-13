2777eef2d6a123d4da5c01a920861b56a908645f
Víctor Fernández Poyatos <vicferpoy@gmail.com>
2020-01-13 13:54:35 +0100

    Add ciscat.get_ciscat_results unit tests

--- framework/wazuh/tests/test_ciscat.py
+++ framework/wazuh/tests/test_ciscat.py
@@ -1,26 +1,60 @@
 #!/usr/bin/env python
-# Copyright (C) 2015-2019, Wazuh Inc.
+# Copyright (C) 2015-2020, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
+import os
+import sys
+from functools import wraps
+from sqlite3 import connect
+from unittest.mock import patch, MagicMock
 
+import pytest
 
-from unittest.mock import patch
 with patch('wazuh.common.ossec_uid'):
     with patch('wazuh.common.ossec_gid'):
-        from wazuh import ciscat
+        sys.modules['api'] = MagicMock()
+        sys.modules['wazuh.rbac.orm'] = MagicMock()
+        import wazuh.rbac.decorators
+
+        del sys.modules['wazuh.rbac.orm']
+        del sys.modules['api']
+
+        def RBAC_bypasser(**kwargs):
+            def decorator(f):
+                @wraps(f)
+                def wrapper(*args, **kwargs):
+                    return f(*args, **kwargs)
+
+                return wrapper
+            return decorator
+
+        wazuh.rbac.decorators.expose_resources = RBAC_bypasser
+        from wazuh.ciscat import get_ciscat_results
+        from wazuh.results import AffectedItemsWazuhResult
 
 
-@patch("wazuh.ciscat.get_item_agent", return_value=None)
-def test_get_ciscat_agent(mock_response):
-    """
-        Tests get_netiface_agent method
-    """
-    ciscat.get_results_agent('001')
+test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+agents_info = ['001', '002']
 
 
-@patch("wazuh.ciscat._get_agent_items", return_value=None)
-def test_get_ciscat_result(mock_response):
-    """
-        Tests get_packages method
-    """
-    ciscat.get_ciscat_results()
+# Get a fake database
+def get_fake_syscheck_db(sql_file):
+    def create_memory_db(*args, **kwargs):
+        syscheck_db = connect(':memory:')
+        cur = syscheck_db.cursor()
+        with open(os.path.join(test_data_path, sql_file)) as f:
+            cur.executescript(f.read())
+        return syscheck_db
+
+    return create_memory_db
+
+
+@pytest.mark.parametrize('agent_id', [
+    ['001']
+])
+@patch('wazuh.ciscat.get_agents_info', return_value=agents_info)
+@patch('sqlite3.connect', side_effect=get_fake_syscheck_db('schema_ciscat_test.sql'))
+@patch('socket.socket.connect')
+def test_get_ciscat_agent(socket_mock, db_mock, agents_info_mock, agent_id):
+    result = get_ciscat_results(agent_id)
+    assert isinstance(result, AffectedItemsWazuhResult)
